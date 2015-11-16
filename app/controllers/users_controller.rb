d3fd3d3c865ffc120e32e23a1b51b128be628f75
PYARIA <jadetong@gmail.com>
2015-11-16 09:52:07 -0800

    Users account creation implemented

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "User account created!"
+    else
+      render :new, alert: "User account not created!"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit([:first_name, :last_name, :email, :password, :password_confirmation])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'ideas#index'
   resources :ideas, only: [:index, :new, :create]
+  resources :users, only: [:new, :create]
 end
